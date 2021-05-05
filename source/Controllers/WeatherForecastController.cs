c7ee2c159707fdb47f0bdfae555364e8a8bf2fd6
kazenetu <rakudafactory@gmail.com>
2021-05-05 23:41:04 +0900

    POST確認用メソッドを追加 #1

--- source/Controllers/WeatherForecastController.cs
+++ source/Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace source.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
