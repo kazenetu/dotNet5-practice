e48171376af6d60ef18e7fa89f511d7c19843e73
kazenetu <rakudafactory@gmail.com>
2021-05-07 00:00:59 +0900

    サーバーサイドのXSRF対策 #2

--- source/Startup.cs
+++ source/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using System;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+
 namespace source
 {
     public class Startup
@@ -26,10 +30,34 @@ namespace source
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            // セッションの設定
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            // session
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                // options.IdleTimeout = TimeSpan.FromSeconds(10);
+                // options.Cookie.HttpOnly = true;
+
+                options.Cookie.Name = "sid";
+            });
+
+            // トークン設定
+            services.AddAntiforgery(options => {
+                options.HeaderName = "X-XSRF-TOKEN";
+                options.Cookie.Path = "/";
+            });
+
+            // トークンキーを発行
+            services.AddMvc(options =>
+                options.Filters.Add(new Microsoft.AspNetCore.Mvc.AutoValidateAntiforgeryTokenAttribute()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IAntiforgery antiforgery)
         {
             if (env.IsDevelopment())
             {
@@ -41,6 +69,25 @@ namespace source
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // session有効化
+            app.UseSession();
+
+            // ルートアクセス時にトークン発行
+            app.Use(next => context =>
+            {
+                if (
+                    string.Equals(context.Request.Path.Value, "/", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(context.Request.Path.Value, "/index.html", StringComparison.OrdinalIgnoreCase))
+                {
+                // We can send the request token as a JavaScript-readable cookie, and Angular will use it by default.
+                var tokens = antiforgery.GetAndStoreTokens(context);
+                context.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
+                            new CookieOptions() { HttpOnly = false });
+                }
+
+                return next(context);
+            });
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
