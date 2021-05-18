97b82a10127d2bcf43ebbe9fd9ead1067faa0f35
kazenetu <rakudafactory@gmail.com>
2021-05-18 21:58:44 +0900

    interfacesのIWeatherForecastを利用する実装に変更 #6

--- source/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ source/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,31 +1,26 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IWeatherForecast } from '../interfaces/IWeatherForecast'
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public forecasts: IWeatherForecast[];
   private httpClient:HttpClient = null;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.httpClient = http;
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+    http.get<IWeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
 
   postRequest(){
-    this.httpClient.post<WeatherForecast[]>("/" + 'weatherforecast',null).subscribe(result => {
+    this.httpClient.post<IWeatherForecast[]>("/" + 'weatherforecast',null).subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
